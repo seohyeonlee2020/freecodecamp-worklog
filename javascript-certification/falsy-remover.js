df3c1d11a26eb9682270ec5c28c0d5924dfb38a5
seohyeon-lee-2025 <seohyeonlee2020@gmail.com>
2026-01-23 11:20:53 +0900

    falsy remover

---
+++ javascript-certification/falsy-remover.js
@@ -0,0 +1,8 @@
+// https://www.freecodecamp.org/learn/javascript-v9/lab-falsy-remover/implement-a-falsy-remover
+// 01/23/2026
+
+const bouncer = (arr) => {
+
+  const filtered = arr.filter((item) => item)
+  return filtered
+}
