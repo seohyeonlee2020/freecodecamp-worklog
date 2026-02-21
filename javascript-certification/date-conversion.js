17ee2ad28e05b2f20fa0834ce87f50fa64a5ed1b
seohyeon-lee-2025 <seohyeonlee2020@gmail.com>
2026-02-21 15:57:59 +0900

    saving work

---
+++ javascript-certification/date-conversion.js
@@ -0,0 +1,26 @@
+const currentDate = new Date()
+
+const currentDateFormat = `Current Date and Time: ${currentDate}`
+
+console.log(currentDateFormat)
+
+const formatDateMMDDYYYY = (obj) => {
+  //convert to month date yr
+  const formatted = obj.toLocaleDateString("en-US")
+
+  return `Formatted Date (MM/DD/YYYY): ${formatted}`
+}
+
+console.log(formatDateMMDDYYYY(currentDate))
+
+const formatDateLong = (obj) => {
+  //convert to month date yr
+    const options = {
+        year: "numeric",
+  month: "long",
+  day: "numeric",
+    }
+    const formatted = obj.toLocaleDateString("en-US", options)
+
+  return ` Formatted Date (Month Day, Year): ${formatted}`
+}
